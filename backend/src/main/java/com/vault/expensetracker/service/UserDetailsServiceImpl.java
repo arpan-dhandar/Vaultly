92fdc12fc9baabbce91a088d9058447520a07768
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-04-14 15:31:01 +0530

    feat: implement UserDetailsService and authentication DTOs for user management

--- backend/src/main/java/com/vault/expensetracker/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/vault/expensetracker/service/UserDetailsServiceImpl.java
@@ -1,5 +1,23 @@
 package com.vault.expensetracker.service;
 
-public class UserDetailsServiceImpl {
-    
+import com.vault.expensetracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> User.withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .authorities("ROLE_USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
 }
